2569732dd430c4fe80f107b4fc789e5897399fe1
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2014-01-20 09:13:07 +0000

    third test added

--- spec/FizzBuzz_spec.rb
+++ spec/FizzBuzz_spec.rb
@@ -9,5 +9,9 @@ describe "FizzBuzz" do
 	it "should return Fizz if it's passed 3" do 
 		expect(fizzbuzz(3)).to eq("Fizz")
 	end
+
+	it "should return Buzz if it;s passed 5" do
+		expect(fizzbuzz(5)).to eq("Buzz")
+	end
 end
 
