67513927aec3c99059aa9c1552cfb63036ab1f65
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2014-01-20 13:32:26 +0000

    code refracted

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,11 +1,23 @@
+def is_divisible_by_three?(n)
+	true if n % 3 == 0
+end
+
+def is_divisible_by_five?(n)
+	true if n % 5 == 0
+end
+
+def is_divisible_by_fifteen?(n)
+	true if n % 15 == 0
+end
+
 def fizzbuzz(n)
-	if n % 15 == 0
+	if is_divisible_by_fifteen?(n)
 		"FizzBuzz"
-		elsif n % 3 == 0
+	elsif is_divisible_by_three?(n)
 		"Fizz"
-		elsif n % 5  == 0
+	elsif is_divisible_by_five?(n)
 		"Buzz"
-		else
+	else
 		n
 	end
 end
