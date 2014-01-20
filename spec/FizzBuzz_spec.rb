8fe5ada383596103454448ac5c9565961303fd79
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2014-01-20 09:20:40 +0000

    fourth test added

--- spec/FizzBuzz_spec.rb
+++ spec/FizzBuzz_spec.rb
@@ -10,8 +10,12 @@ describe "FizzBuzz" do
 		expect(fizzbuzz(3)).to eq("Fizz")
 	end
 
-	it "should return Buzz if it;s passed 5" do
+	it "should return Buzz if it's passed 5" do
 		expect(fizzbuzz(5)).to eq("Buzz")
+	end
+
+	it "should return FizzBuzz if the number is divisible by 3 or 5" do
+		expect(fizzbuzz(15)).to eq("FizzBuzz")
 	end
 end
 
