983d3b8ead91a4b7957634a88f42a66722e1a88c
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2014-01-21 18:08:25 +0000

    final touches

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,23 +1,23 @@
-def is_divisible_by_three?(n)
-	true if n % 3 == 0
+def is_divisible_by_3?(n)
+	true if n%3 == 0
 end
 
-def is_divisible_by_five?(n)
-	true if n % 5 == 0
+def is_divisible_by_5?(n)
+	true if n%5 == 0
 end
 
-def is_divisible_by_fifteen?(n)
-	true if n % 15 == 0
+def is_divisible_by_15?(n)
+	true if n%15 == 0
 end
 
-def fizzbuzz(n)
-	if is_divisible_by_fifteen?(n)
-		"FizzBuzz"
-	elsif is_divisible_by_three?(n)
-		"Fizz"
-	elsif is_divisible_by_five?(n)
-		"Buzz"
-	else
-		n
-	end
+def Fizzbuzz(n)
+	return "FizzBuzz" if is_divisible_by_15?(n)
+	return "Buzz" if is_divisible_by_5?(n)
+	return "Fizz" if is_divisible_by_3?(n)
+	n
 end
+
+p Fizzbuzz(3)
+
+
+
--- spec/FizzBuzz_spec.rb
+++ spec/FizzBuzz_spec.rb
@@ -1,21 +1,55 @@
-require './FizzBuzz'
+require './fizzbuzz.rb'
 
 
-describe "FizzBuzz" do 
-	it "should return 1 if it's passed 1" do 
-		expect(fizzbuzz(1)).to eq (1)
+describe 'Fizzbuzz' do
+
+
+	context "In order 'Fizzbuzz' to run" do
+
+	it "should tell if a number is divisible by 3" do
+		expect(is_divisible_by_3?(3)).to be_true
+	  end
+
+	 it "should tell if a number is not divisible by 3" do
+	  expect(is_divisible_by_3?(2)).to be_false
+		end
+
+		it "should tell if a number is divisible by 5" do
+			expect(is_divisible_by_5?(5)).to be_true
+		end
+
+		it "should tell if a number is not divisible by 5" do
+			expect(is_divisible_by_5?(4)).to be_false
+		end
+
+		it "should tell if a number is divisible by 3 and 5" do
+			expect(is_divisible_by_15?(15)).to be_true 
+		end
+
+		it "should tell if number is not divisible by 3 and 5" do
+			expect(is_divisible_by_15?(1)).to be_false
+		end
+	
 	end
 
-	it "should return Fizz if it's passed 3" do 
-		expect(fizzbuzz(3)).to eq("Fizz")
+	context "When we play Fizzbuzz" do
+
+		it "should return 1 when we pass number 1" do
+			expect(Fizzbuzz(1)).to eq(1)
+		end
+
+		it "should return Fizz when we pass number 3" do
+			expect(Fizzbuzz(3)).to eq('Fizz')
+		end
+
+		it "should return Buzz when we pass number 5" do
+			expect(Fizzbuzz(5)).to eq('Buzz')
+		end
+		
+		it "should return Fizzbuzz when we pass number 15" do
+			expect(Fizzbuzz(15)).to eq('FizzBuzz')
+		end
+
 	end
 
-	it "should return Buzz if it's passed 5" do
-		expect(fizzbuzz(5)).to eq("Buzz")
-	end
-
-	it "should return FizzBuzz if the number is divisible by 3 or 5" do
-		expect(fizzbuzz(15)).to eq("FizzBuzz")
-	end
 end
-
